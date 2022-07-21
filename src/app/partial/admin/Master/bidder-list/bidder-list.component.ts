1c5742029ba8d0ccef89d1a24edaddaf35464fd4
yogesh7355 <yogesh.jadhav@shauryatechnosoft.com>
2022-07-21 16:26:47 +0530

    remove unwanted import

--- src/app/partial/admin/Master/bidder-list/bidder-list.component.ts
+++ src/app/partial/admin/Master/bidder-list/bidder-list.component.ts
@@ -1,18 +1,14 @@
-import { SelectionModel } from '@angular/cdk/collections';
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, UntypedFormControl } from '@angular/forms';
-import { MatDialog } from '@angular/material/dialog';
+
+import { Component,  OnInit,  } from '@angular/core';
+import { UntypedFormControl } from '@angular/forms';
+
 import { MatFormFieldDefaultOptions, MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSelectChange } from '@angular/material/select';
-import { MatTableDataSource } from '@angular/material/table';
-import { Observable, ReplaySubject } from 'rxjs';
 import { fadeInUp400ms } from 'src/@vex/animations/fade-in-up.animation';
 import { stagger40ms } from 'src/@vex/animations/stagger.animation';
-import { TableColumn } from 'src/@vex/interfaces/table-column.interface';
-import { ApiService } from 'src/app/core/services/api.service';
-import { CommonService } from 'src/app/core/services/common.service';
-import { ErrorsService } from 'src/app/core/services/errors.service';
+
+
+
+
 
 @Component({
   selector: 'vex-bidder-list',
