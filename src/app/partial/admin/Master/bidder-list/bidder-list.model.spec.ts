d23f7cd1dfbeb7f64a4d66ad1b9504549fc8a70a
sham <sham.jadhav@shauryatechnosoft.com>
2022-07-21 18:57:09 +0530

    add dash pipe

---
+++ src/app/partial/admin/Master/bidder-list/bidder-list.model.spec.ts
@@ -0,0 +1,7 @@
+import { BidderList } from './bidder-list.model';
+
+describe('BidderList', () => {
+  it('should create an instance', () => {
+    expect(new BidderList()).toBeTruthy();
+  });
+});
