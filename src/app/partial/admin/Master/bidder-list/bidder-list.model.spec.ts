e1595c30ad727c6316ee72c8960b805437a88481
sham <sham.jadhav@shauryatechnosoft.com>
2022-07-22 10:43:55 +0530

    add debuggr list

--- src/app/partial/admin/Master/bidder-list/bidder-list.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { BidderList } from './bidder-list.model';
-
-describe('BidderList', () => {
-  it('should create an instance', () => {
-    expect(new BidderList()).toBeTruthy();
-  });
-});
