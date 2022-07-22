e334f531209fd55d196c19d1bf686de2ab51988e
sham <sham.jadhav@shauryatechnosoft.com>
2022-07-22 10:39:02 +0530

    get pull

--- src/app/partial/admin/Master/bidder-list/bidder-list.component.ts
+++ src/app/partial/admin/Master/bidder-list/bidder-list.component.ts
@@ -12,6 +12,7 @@ import { ReplaySubject, Subscription } from 'rxjs';
 import { fadeInUp400ms } from 'src/@vex/animations/fade-in-up.animation';
 import { stagger40ms } from 'src/@vex/animations/stagger.animation';
 import { TableColumn } from 'src/@vex/interfaces/table-column.interface';
+import { ConfirmationDialogComponent } from 'src/app/core/dialogs/confirmation-dialog/confirmation-dialog.component';
 import { ApiService } from 'src/app/core/services/api.service';
 import { CommonService } from 'src/app/core/services/common.service';
 import { ErrorsService } from 'src/app/core/services/errors.service';
@@ -52,15 +53,15 @@ export class BidderListComponent implements OnInit {
   @Input()
   pageNumber: number = 1;
   columns: TableColumn<UserRegistration>[] = [
-    { label: 'Sr.No', property: 'srNo', type: 'button', visible: true },
-    { label: 'Name', property: 'name', type: 'text', visible: true, cssClasses: ['font-medium'] },
-    { label: 'Role', property: 'roleType', type: 'text', visible: true },
-    { label: 'Mobile', property: 'mobileNo', type: 'text', visible: true },
-    { label: 'User Type', property: 'userType', type: 'text', visible: true },
-    { label: 'Sub User Type', property: 'subUserType', type: 'text', visible: true },
-    { label: 'DSC Status', property: 'isDsc', type: 'button', visible: true, cssClasses: ['text-center'] },
-    { label: 'Block /Unblock', property: 'isBlock', type: 'button', visible: true },
-    { label: 'Actions', property: 'actions', type: 'button', visible: true },
+    { label: 'Sr.No', property: 'srNo', type: 'button', visible: true, cssClasses: ['text-secondary', 'font-medium'] },
+    { label: 'Name', property: 'name', type: 'text', visible: true },
+    { label: 'District', property: 'district', type: 'text', visible: true, cssClasses: ['text-secondary', 'font-medium'] },
+    { label: 'Mobile', property: 'mobile', type: 'text', visible: true, cssClasses: ['text-secondary', 'font-medium'] },
+    { label: 'Bidder Type', property: 'bidderType', type: 'text', visible: true, cssClasses: ['text-secondary', 'font-medium'] },
+
+    { label: 'Dsc Status', property: 'isDsc', type: 'text', visible: true, cssClasses: ['text-secondary', 'font-medium'] },
+    { label: 'Unblock Block', property: 'isBlock', type: 'button', visible: true, cssClasses: ['text-secondary', 'font-medium'] },
+    { label: 'Actions', property: 'actions', type: 'button', visible: true, cssClasses: ['text-secondary', 'font-medium'] },
   ];
 
   filterForm!: FormGroup;
@@ -69,8 +70,8 @@ export class BidderListComponent implements OnInit {
   constructor(public dialog: MatDialog,
     private apiService: ApiService, private fb: FormBuilder,
     public commonService: CommonService,
-    private localstorageService:LocalstorageService,
-    private error: ErrorsService, private masterService:MasterService) { }
+    private localstorageService: LocalstorageService,
+    private error: ErrorsService, private masterService: MasterService) { }
 
   get visibleColumns() {
     return this.columns.filter(column => column.visible).map(column => column.property);
@@ -146,7 +147,7 @@ export class BidderListComponent implements OnInit {
     column.visible = !column.visible;
   }
 
- 
+
   // pagination code start here //
   pageChanged(event: any) {
     this.pageNumber = event.pageIndex + 1;
@@ -161,7 +162,45 @@ export class BidderListComponent implements OnInit {
       data: '',
     });
     dialogRef.afterClosed().subscribe(result => {
-      console.log(`Dialog result: ${result}`); 
+      console.log(`Dialog result: ${result}`);
     });
+  }
+
+  userBlockUnBlockModal(element: any, event: any) {
+    let Title: string, dialogText: string;
+    event.checked == true ? Title = 'User Block' : Title = 'User Unblock';
+    event.checked == true ? dialogText = 'Do you want to User Block' : dialogText = 'Do you want to User Unblock';
+    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+      width: '340px',
+      data: { p1: dialogText, p2: '', cardTitle: Title, successBtnText: 'Yes', dialogIcon: 'done_outline', cancelBtnText: 'No' },
+      disableClose: this.apiService.disableCloseFlag,
+    });
+    dialogRef.afterClosed().subscribe((res: any) => {
+      res == 'Yes' ? this.userBlockUnBlock(element, event.checked) : !event.checked ? event.source.checked = true : event.source.checked = false;
+    });
+  }
+
+  userBlockUnBlock(element: any, event: any) {
+    let obj = {
+      "id": element?.userId,
+      "isBlock": event == true ? true : false,
+      "blockDate": new Date(),
+      "blockBy": this.localstorageService.userId(),
+      "blockRemark": ""
+    }
+    this.apiService.setHttp('PUT', "user-registration/BlockUnblockUser", false, JSON.stringify(obj), false, 'masterUrl');
+    this.subscription = this.apiService.getHttp().subscribe({
+      next: (res: any) => {
+        if (res.statusCode === "200") {
+          this.getData();
+          this.commonService.snackBar(res.statusMessage, 0);
+        } else {
+          if (res.statusCode != "404") {
+            this.commonService.checkDataType(res.statusMessage) == false ? this.error.handelError(res.statusCode) : this.commonService.snackBar(res.statusMessage, 1);
+          }
+        }
+      },
+      error: (err: any) => { this.error.handelError(err) }
+    })
   }
 }
