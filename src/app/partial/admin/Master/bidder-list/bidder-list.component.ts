ca8374717167ea02aa4472bd30f456623387d9ae
somnath khabale <somnathkhabale1998@gmail.com>
2022-07-22 14:17:17 +0530

    changes in bidder list and user reg pages

--- src/app/partial/admin/Master/bidder-list/bidder-list.component.ts
+++ src/app/partial/admin/Master/bidder-list/bidder-list.component.ts
@@ -5,10 +5,9 @@ import { FormBuilder, FormGroup, UntypedFormControl } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { MatFormFieldDefaultOptions, MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { MatPaginator } from '@angular/material/paginator';
-import { MatSelectChange } from '@angular/material/select';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
-import { ReplaySubject } from 'rxjs';
+import { ReplaySubject, Subscription } from 'rxjs';
 import { fadeInUp400ms } from 'src/@vex/animations/fade-in-up.animation';
 import { stagger40ms } from 'src/@vex/animations/stagger.animation';
 import { TableColumn } from 'src/@vex/interfaces/table-column.interface';
@@ -16,7 +15,9 @@ import { ConfirmationDialogComponent } from 'src/app/core/dialogs/confirmation-d
 import { ApiService } from 'src/app/core/services/api.service';
 import { CommonService } from 'src/app/core/services/common.service';
 import { ErrorsService } from 'src/app/core/services/errors.service';
-import { UserRegistration } from '../user-registration/user-registration.model';
+import { LocalstorageService } from 'src/app/core/services/localstorage.service';
+import { AddBidderComponent } from './add-bidder/add-bidder.component';
+import { BidderList } from './bidder-list.model';
 
 @Component({
   selector: 'vex-bidder-list',
@@ -39,15 +40,16 @@ export class BidderListComponent implements OnInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
   layoutCtrl = new UntypedFormControl('boxed');
-  selection = new SelectionModel<UserRegistration>(true, []);
+  selection = new SelectionModel<BidderList>(true, []);
   searchCtrl = new UntypedFormControl();
-  dataSource: MatTableDataSource<UserRegistration> | null;
-  userRegistration: UserRegistration[];
-  subject$: ReplaySubject<UserRegistration[]> = new ReplaySubject<UserRegistration[]>(1);
+  dataSource: MatTableDataSource<BidderList> | null;
+  userRegistration: BidderList[];
+  subject$: ReplaySubject<BidderList[]> = new ReplaySubject<BidderList[]>(1);
+  subscription!: Subscription;
 
   @Input()
   pageNumber: number = 1;
-  columns: TableColumn<UserRegistration>[] = [
+  columns: TableColumn<BidderList>[] = [
     { label: 'Sr.No', property: 'srNo', type: 'button', visible: true, cssClasses: ['text-secondary', 'font-medium'] },
     { label: 'Name', property: 'name', type: 'text', visible: true },
     { label: 'District', property: 'district', type: 'text', visible: true, cssClasses: ['text-secondary', 'font-medium'] },
@@ -66,7 +68,7 @@ export class BidderListComponent implements OnInit {
     private apiService: ApiService, private fb: FormBuilder,
     public commonService: CommonService,
     private localstorageService: LocalstorageService,
-    private error: ErrorsService, private masterService: MasterService) { }
+    private error: ErrorsService) { }
 
   get visibleColumns() {
     return this.columns.filter(column => column.visible).map(column => column.property);
@@ -82,11 +84,9 @@ export class BidderListComponent implements OnInit {
 
   defultFilterform() {
     this.filterForm = this.fb.group({
-      stateId: [],
-      divisionId: [0],
       districtId: [0],
-      talukaId: [0],
       search: [''],
+      bidderType: ['All'],
     });
   }
   createUser(): void {
@@ -98,33 +98,36 @@ export class BidderListComponent implements OnInit {
     });
   }
 
-  deleteCustomers(_customers: UserRegistration[]) {
-    // this.userRegistration.splice(this.userRegistration.findIndex((existingCustomer) => existingCustomer.id === customer.id), 1);
-    // this.selection.deselect(customers);
-    this.subject$.next(this.userRegistration);
+  getData() {
+    let localstorData = this.localstorageService.getLoggedInLocalstorageData().responseData;
+    let formValue = this.filterForm.value;
+    let bidderTy = formValue.bidderType == 'All' ? '' : formValue.bidderType;
+    let obj = "StateId=" + localstorData?.stateId + "&DivisionId=" + localstorData?.divisionId + "&DistrictId=" + formValue.districtId +
+      "&BidderType=" + bidderTy + "&ProjectId=" + localstorData?.projectId + "&pageno=" + this.pageNumber + "&pagesize=" + 10;
+    this.commonService.checkDataType(formValue.search) == true ? obj += "&Textsearch=" + formValue.search : '';
+    this.apiService.setHttp('get', "user-registration/GetBidderUsers?" + obj, false, false, false, 'masterUrl');
+    this.subscription = this.apiService.getHttp().subscribe({
+      next: (res: any) => {
+        if (res.statusCode === "200") {
+          this.dataSource = new MatTableDataSource(res.responseData.responseData1);
+          this.dataSource.sort = this.sort;
+
+          this.totalRows = res.responseData.responseData2.pageCount;
+          this.totalRows > 10 && this.pageNumber == 1 ? this.paginator?.firstPage() : '';
+        } else {
+          this.totalRows = 0;
+          if (res.statusCode != "404") {
+            this.commonService.checkDataType(res.statusMessage) == false ? this.error.handelError(res.statusCode) : this.commonService.snackBar(res.statusMessage, 1);
+          }
+        }
+      },
+      error: ((error: any) => { this.error.handelError(error.status) })
+    });
   }
 
-  /** Whether the number of selected elements matches the total number of rows. */
-  isAllSelected() {
-    const numSelected = this.selection.selected.length;
-    const numRows = this.dataSource.data.length;
-    return numSelected === numRows;
-  }
-
-  masterToggle() {
-    this.isAllSelected() ?
-      this.selection.clear() :
-      this.dataSource.data.forEach(row => this.selection.select(row));
-  }
 
   trackByProperty<T>(_index: number, column: TableColumn<T>) {
     return column.property;
-  }
-
-  onLabelChange(change: MatSelectChange, row: UserRegistration) {
-    const index = this.userRegistration.findIndex(c => c === row);
-    this.userRegistration[index].labels = change.value;
-    this.subject$.next(this.userRegistration);
   }
 
   toggleColumnVisibility(column, event) {
@@ -133,7 +136,6 @@ export class BidderListComponent implements OnInit {
     column.visible = !column.visible;
   }
 
-  // pagination code start here //
   pageChanged(event: any) {
     this.pageNumber = event.pageIndex + 1;
     this.getData();
