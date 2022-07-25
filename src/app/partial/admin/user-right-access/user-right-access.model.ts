ddabe6d2c555ec342c1bf3fcc58338aa6971c95b
yogesh7355 <yogesh.jadhav@shauryatechnosoft.com>
2022-07-25 10:43:04 +0530

    user right access page complete

---
+++ src/app/partial/admin/user-right-access/user-right-access.model.ts
@@ -0,0 +1,21 @@
+export class UserRightAccess {
+designation: null
+designationId: number
+id: number
+isDSC: boolean
+pageId: number
+pageName: string
+pageNameView: string
+pageType: number
+pageURL: string
+readRight: boolean
+roleId: number
+roleType: string
+subUserType: string
+subUserTypeId: number
+userTypeId: number
+writeRight: boolean
+
+labels: any;
+action:any;
+}
