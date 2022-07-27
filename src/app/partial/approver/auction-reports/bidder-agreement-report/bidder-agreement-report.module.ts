82eeca69bf225a0465587ef1712cd6b35b6ce901
Rahulmistari2141 <rahulmistari2141@gmail.com>
2022-07-27 16:28:26 +0530

    Approver-post-auction,bid-history,bidder-agreement

--- src/app/partial/approver/auction-reports/bidder-agreement-report/bidder-agreement-report.module.ts
+++ src/app/partial/approver/auction-reports/bidder-agreement-report/bidder-agreement-report.module.ts
@@ -4,6 +4,10 @@ import { CommonModule } from '@angular/common';
 import { BidderAgreementReportRoutingModule } from './bidder-agreement-report-routing.module';
 import { BidderAgreementReportComponent } from './bidder-agreement-report.component';
 import { AngularMaterialModule } from 'src/app/core/angular-material/angular-material.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BreadcrumbsModule } from 'src/@vex/components/breadcrumbs/breadcrumbs.module';
+import { PageLayoutModule } from 'src/@vex/components/page-layout/page-layout.module';
+import { SecondaryToolbarModule } from 'src/@vex/components/secondary-toolbar/secondary-toolbar.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +16,11 @@ import { AngularMaterialModule } from 'src/app/core/angular-material/angular-mat
   imports: [
     CommonModule,
     BidderAgreementReportRoutingModule,
-    AngularMaterialModule
+    AngularMaterialModule,
+    ReactiveFormsModule,
+    BreadcrumbsModule,
+    PageLayoutModule,
+    SecondaryToolbarModule
   ]
 })
 export class BidderAgreementReportModule { }
