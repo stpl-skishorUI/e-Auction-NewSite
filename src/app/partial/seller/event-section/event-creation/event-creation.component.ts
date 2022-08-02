b7e662f2ebf78c0cc79ec385817c9f0405e76dc1
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2022-08-02 16:29:05 +0530

    event creation page designed

--- src/app/partial/seller/event-section/event-creation/event-creation.component.ts
+++ src/app/partial/seller/event-section/event-creation/event-creation.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UntypedFormControl } from '@angular/forms';
 @Component({
   selector: 'vex-event-creation',
   templateUrl: './event-creation.component.html',
   styleUrls: ['./event-creation.component.scss']
 })
 export class EventCreationComponent implements OnInit {
-
+  layoutCtrl = new UntypedFormControl('boxed');
   constructor() { }
 
   ngOnInit(): void {
--- src/app/partial/seller/event-section/event-creation/event-creation.module.ts
+++ src/app/partial/seller/event-section/event-creation/event-creation.module.ts
@@ -3,7 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { EventCreationRoutingModule } from './event-creation-routing.module';
 import { EventCreationComponent } from './event-creation.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { BreadcrumbsModule } from 'src/@vex/components/breadcrumbs/breadcrumbs.module';
+import { PageLayoutModule } from 'src/@vex/components/page-layout/page-layout.module';
+import { SecondaryToolbarModule } from 'src/@vex/components/secondary-toolbar/secondary-toolbar.module';
+import { AngularMaterialModule } from 'src/app/core/angular-material/angular-material.module';
 
 @NgModule({
   declarations: [
@@ -11,7 +15,12 @@ import { EventCreationComponent } from './event-creation.component';
   ],
   imports: [
     CommonModule,
-    EventCreationRoutingModule
+    EventCreationRoutingModule,
+    ReactiveFormsModule,
+    BreadcrumbsModule,
+    PageLayoutModule,
+    SecondaryToolbarModule,
+    AngularMaterialModule
   ]
 })
 export class EventCreationModule { }
