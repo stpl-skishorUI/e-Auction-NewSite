92cee44e06ff32b90d66a923c740448618c3e1b5
Rahulmistari2141 <rahulmistari2141@gmail.com>
2022-08-08 11:51:37 +0530

    approve-event page redesign

--- src/app/partial/approver/event-approval/approve-event/approve-event.module.ts
+++ src/app/partial/approver/event-approval/approve-event/approve-event.module.ts
@@ -5,6 +5,10 @@ import { ApproveEventRoutingModule } from './approve-event-routing.module';
 import { ApproveEventComponent } from './approve-event.component';
 import { AngularMaterialModule } from 'src/app/core/angular-material/angular-material.module';
 
+import { SecondaryToolbarModule } from 'src/@vex/components/secondary-toolbar/secondary-toolbar.module';
+import { PageLayoutModule } from 'src/@vex/components/page-layout/page-layout.module';
+import { BreadcrumbsModule } from 'src/@vex/components/breadcrumbs/breadcrumbs.module';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     ApproveEventComponent
@@ -12,7 +16,11 @@ import { AngularMaterialModule } from 'src/app/core/angular-material/angular-mat
   imports: [
     CommonModule,
     ApproveEventRoutingModule,
-    AngularMaterialModule
+    AngularMaterialModule,
+    SecondaryToolbarModule,
+    PageLayoutModule,
+    BreadcrumbsModule,
+    ReactiveFormsModule
   ]
 })
 export class ApproveEventModule { }
