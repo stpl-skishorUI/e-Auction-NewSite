2cae534ca37acc9a0449f76134bbee893b3a3b16
Rahulmistari2141 <rahulmistari2141@gmail.com>
2022-08-12 17:13:55 +0530

    All pages redesign and changes filter

--- src/app/partial/approver/seller/agreement/agreement.module.ts
+++ src/app/partial/approver/seller/agreement/agreement.module.ts
@@ -4,6 +4,10 @@ import { CommonModule } from '@angular/common';
 import { AgreementRoutingModule } from './agreement-routing.module';
 import { AgreementComponent } from './agreement.component';
 import { AngularMaterialModule } from 'src/app/core/angular-material/angular-material.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BreadcrumbsModule } from 'src/@vex/components/breadcrumbs/breadcrumbs.module';
+import { PageLayoutModule } from 'src/@vex/components/page-layout/page-layout.module';
+import { SecondaryToolbarModule } from 'src/@vex/components/secondary-toolbar/secondary-toolbar.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +16,11 @@ import { AngularMaterialModule } from 'src/app/core/angular-material/angular-mat
   imports: [
     CommonModule,
     AgreementRoutingModule,
-    AngularMaterialModule
+    AngularMaterialModule,
+    ReactiveFormsModule,
+    BreadcrumbsModule,
+    PageLayoutModule,
+    SecondaryToolbarModule,
   ]
 })
 export class AgreementModule { }
--- src/app/partial/bidder/account-details/account-details.module.ts
+++ src/app/partial/bidder/account-details/account-details.module.ts
@@ -4,6 +4,10 @@ import { CommonModule } from '@angular/common';
 import { AccountDetailsRoutingModule } from './account-details-routing.module';
 import { AccountDetailsComponent } from './account-details.component';
 
+import { PageLayoutModule } from 'src/@vex/components/page-layout/page-layout.module';
+import { BreadcrumbsModule } from 'src/@vex/components/breadcrumbs/breadcrumbs.module';
+import { SecondaryToolbarModule } from 'src/@vex/components/secondary-toolbar/secondary-toolbar.module';
+import { AngularMaterialModule } from 'src/app/core/angular-material/angular-material.module';
 
 @NgModule({
   declarations: [
@@ -11,7 +15,11 @@ import { AccountDetailsComponent } from './account-details.component';
   ],
   imports: [
     CommonModule,
-    AccountDetailsRoutingModule
+    AccountDetailsRoutingModule,
+    AngularMaterialModule,
+    PageLayoutModule,
+    BreadcrumbsModule,
+    SecondaryToolbarModule
   ]
 })
 export class AccountDetailsModule { }
